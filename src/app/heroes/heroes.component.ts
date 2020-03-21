9f9f903bd7a914492e683f72ec87817dab42e49d
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-03-21 13:34:52 -0300

    (refactor): adding service in heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,31 @@
-import { Hero } from './../hero';
-import { HEROES } from './../mock-heroes';
-import { Component, OnInit } from '@angular/core';
+import { MessageService } from "./../services/message.service";
+import { Hero } from "./../interfaces/hero";
+import { HeroService } from "./../services/hero.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES; 
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messsageService: MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messsageService.add(`HeroService: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
